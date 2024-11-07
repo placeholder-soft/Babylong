106b85bc2cee3a069dba58577b15398e3b041ad7
chuan_yu <isyuchuan@foxmail.com>
2024-11-07 16:17:07 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,9 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
+      <div className='font-bold'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='font-bold'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
