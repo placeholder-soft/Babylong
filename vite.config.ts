098a4520e916b0541c2fa8c39037da7a14942831
Bingjian <bing@placeholdersoft.com>
2024-11-08 16:41:13 +0800

    chore: update lock

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,24 @@
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
+import CloudflarePagesFunctions from "vite-plugin-cloudflare-functions"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
 import svgr from "vite-plugin-svgr"
-import CloudflarePagesFunctions from "vite-plugin-cloudflare-functions"
-import type { Plugin } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), nodePolyfills(), CloudflarePagesFunctions({
-    root: "./functions",
-            outDir: undefined,
-            dts: "./cloudflare.d.ts",
-            wrangler: {
-              log: true,
-              kv: ["satlayer_hackathon_tokens"],
-            },
-    })
+  plugins: [
+    react(),
+    svgr(),
+    nodePolyfills(),
+    CloudflarePagesFunctions({
+      root: "./functions",
+      outDir: undefined,
+      dts: "./cloudflare.d.ts",
+      wrangler: {
+        log: true,
+        kv: ["satlayer_hackathon_tokens"],
+      },
+    }),
   ],
   resolve: {
     alias: {
